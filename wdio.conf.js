944960d5897775c206f9624e541280a052a688b0
Girma Nigusse <xgirma@gmail.com>
2020-03-24 20:28:03 -0400

    updates

--- wdio.conf.js
+++ wdio.conf.js
@@ -5,19 +5,20 @@ const headed = {
   specs: ["./spec/*.spec.js"],
   capabilities: [
     {
-      browserName: "chrome"
-    }
+      browserName: "chrome",
+    },
   ],
   logLevel: "silent",
   services: ["chromedriver"],
   framework: "mocha",
   reporters: ["dot"],
   mochaOpts: {
-    timeout: 60000
+    timeout: 60000,
   },
   before(capabilities, specs) {
+    // eslint-disable-next-line import/no-extraneous-dependencies
     require("@babel/register");
-  }
+  },
 };
 
 const headless = {
@@ -28,23 +29,24 @@ const headless = {
     {
       browserName: "chrome",
       "goog:chromeOptions": {
-        args: ["--headless", "--disable-gpu"]
-      }
-    }
+        args: ["--headless", "--disable-gpu"],
+      },
+    },
   ],
   logLevel: "silent",
   services: ["chromedriver"],
   framework: "mocha",
   reporters: ["dot"],
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 60000
+    defaultTimeoutInterval: 60000,
   },
   mochaOpts: {
-    timeout: 60000
+    timeout: 60000,
   },
   before(capabilities, specs) {
+    // eslint-disable-next-line import/no-extraneous-dependencies
     require("@babel/register");
-  }
+  },
 };
 
 const config = process.env.GITHUB_ACTIONS ? headless : headed;
